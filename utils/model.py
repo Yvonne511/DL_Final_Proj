987d34f1086f5d16493e5eedf910becb5a131151
Yvonne Wu <yw4142@log-1.hpc.nyu.edu>
2024-12-14 22:55:50 -0500

    checkpoint 2

--- utils/model.py
+++ utils/model.py
@@ -73,6 +73,7 @@ class JEPA_Model(nn.Module):
         pred_depth=6,
         pred_emb_dim=384,
         action_dim=2,
+        momentum_scheduler=None,
     ):
         super(JEPA_Model, self).__init__()
 
@@ -106,6 +107,31 @@ class JEPA_Model(nn.Module):
             raise ValueError(f"img_size ({img_size}) must be divisible by patch_size ({patch_size}).")
         self.repr_dim = int((img_size // patch_size) ** 2 * embed_dim)
         self.device = device
+        self.momentum_scheduler = momentum_scheduler or (x for x in [0.999] * 1000)
+
+    def momentum_update(self):
+        with torch.no_grad():
+            m = next(self.momentum_scheduler)  # Get current momentum value
+            for param_q, param_k in zip(self.observation_encoder.parameters(), self.target_encoder.parameters()):
+                param_k.data.mul_(m).add_((1. - m) * param_q.data)
+
+    def variance_loss(self, embeddings, epsilon=1e-4, target_std=0.8):
+        # embeddings: [B, D]
+        # Compute variance along the batch dimension
+        var = embeddings.var(dim=0) + epsilon
+        std = torch.sqrt(var)
+        # Hinge-like penalty to push std above target_std
+        var_loss = torch.mean(nn.functional.relu(target_std - std))
+        return var_loss
+
+    def covariance_loss(self, embeddings, epsilon=1e-4):
+        # embeddings: [B, D]
+        B, D = embeddings.size()
+        embeddings = embeddings - embeddings.mean(dim=0)
+        cov = (embeddings.T @ embeddings) / (B - 1) + epsilon
+        off_diag = cov - torch.diag(torch.diag(cov))
+        cov_loss = off_diag.pow(2).sum() / D
+        return cov_loss
 
     def forward(self, states, actions):
         if self.training:
@@ -122,10 +148,27 @@ class JEPA_Model(nn.Module):
             flattened_emb_hist = states_emb_hist.reshape(B * (T-1), N, D) # [B*(T-1), N, D]
             flattened_actions = actions.reshape(B * (T-1), actions.shape[-1]) # [B*(T-1), A]
             states_emb_pred = self.predictor(flattened_emb_hist, flattened_actions)  # Shape: [B*(T-1), N, D] / [B*(T-1), 2]
+            states_emb_pred = states_emb_pred[:, :-1, :] 
 
-            loss = nn.MSELoss()(states_emb_pred_tgt, states_emb_pred[:, :-1, :])
+            f1_loss = nn.functional.smooth_l1_loss(states_emb_pred_tgt, states_emb_pred)
 
-            return loss
+            states_emb_pred_pooled = states_emb_pred.mean(dim=1)
+            states_emb_pred_tgt_pooled = states_emb_pred_tgt.mean(dim=1) 
+
+            var_loss_pred = self.variance_loss(states_emb_pred_pooled)
+            var_loss_tgt = self.variance_loss(states_emb_pred_tgt_pooled)
+            cov_loss_pred = self.covariance_loss(states_emb_pred_pooled)
+            cov_loss_tgt = self.covariance_loss(states_emb_pred_tgt_pooled)
+
+            var_loss = var_loss_pred + var_loss_tgt
+            cov_loss = cov_loss_pred + cov_loss_tgt
+            lambda_f1 = 10
+            lambda_var = 1
+            lambda_cov = 10
+            loss = lambda_f1 * f1_loss + lambda_var * var_loss + lambda_cov * cov_loss
+
+            self.momentum_update()
+            return loss, f1_loss, var_loss, cov_loss
         else:
             B, T, C, H, W = states.shape # Expecting input [B, 1, C, H, W]
             flattened_states = states.reshape(B * T , C, H, W)  # Flatten to [B, C, H, W]
